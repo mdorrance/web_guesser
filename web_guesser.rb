6b296fb8bdffed133ae059f91bf499f1e46c3773
Michael Dorrance <dorrancemd@gmail.com>
2015-06-30 19:26:06 -0600

    adds color background based on response

--- web_guesser.rb
+++ web_guesser.rb
@@ -20,6 +20,18 @@ before do
 				"Too low!"
 			end
 	end
+
+	def background_color(message)
+		if message == ""
+			"white"
+		elsif message == "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}"
+			"green"			
+		elsif message == "Way too high!" || message == "Way too low!"
+			"red"
+		else message == "Too high!" || message == "Too low!"
+			"blue"
+		end
+	end
 end
 
 number = SECRET_NUMBER
@@ -27,6 +39,7 @@ number = SECRET_NUMBER
 get '/' do
 	guess = params["guess"]
 	message = check_guess(guess)
-  erb :index, :locals => {:number => number, :message => message}	
+	color = background_color(message)
+  erb :index, :locals => {:number => number, :message => message, :color => color}	
 end
 
