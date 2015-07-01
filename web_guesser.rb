18cfb1aa82c885176b106f997e35f2d3c2d0ef45
Michael Dorrance <dorrancemd@gmail.com>
2015-06-30 18:58:55 -0600

    extracts the method

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,32 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
-get '/' do
-  "The SECRET NUMBER is #{number}"
+SECRET_NUMBER = rand(100)
+
+before do
+	def check_guess(guess)
+
+		if guess.to_i == 0
+				""
+			elsif guess.to_i == SECRET_NUMBER
+				"You got it right! The SECRET NUMBER is #{SECRET_NUMBER}"
+			elsif guess.to_i > (SECRET_NUMBER + 5)
+				"Way too high!"
+			elsif guess.to_i > SECRET_NUMBER
+				"Too high!"
+			elsif guess.to_i < (SECRET_NUMBER - 5)
+				"Way too low!"		
+			else guess.to_i < SECRET_NUMBER
+				"Too low!"
+			end
+	end
 end
+
+number = SECRET_NUMBER
+
+get '/' do
+	guess = params["guess"]
+	message = check_guess(guess)
+  erb :index, :locals => {:number => number, :message => message}	
+end
+
